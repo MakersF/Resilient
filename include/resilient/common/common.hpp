9e8475cdd0456e0591f009dd3547a4edf7c297ee
Francesco Zoffoli <makers.f.dev@gmail.com>
2017-12-22 17:34:36 +0000

    Added documentation to variant

---
+++ include/resilient/common/common.hpp
@@ -0,0 +1,7 @@
+/**
+ * @defgroup Common
+ *
+ * @brief Module which contains common classes and functions to operate with the library.
+ *
+ */
+#include <resilient/common/variant.hpp>
--- include/resilient/common/variant.hpp
+++ include/resilient/common/variant.hpp
@@ -16,17 +16,61 @@ static_assert(false, "Only one of FORCE_STD and FORCE_BOOST can be set.")
 
 namespace resilient {
 
+/**
+ * @ingroup Common
+ * @brief A Variant implementation.
+ *
+ * The concept is equivalent with the std::variant from C++17.
+ *
+ * @see http://en.cppreference.com/w/cpp/utility/variant
+ *
+ * @tparam T... The types of the variant
+ */
 template<typename ...T>
 using Variant = detail::Variant<T...>;
 
+/**
+ * @ingroup Common
+ * @brief Check whether a type is a Variant.
+ * @related resilient::Variant
+ *
+ * @tparam T The type to check
+ */
 template<typename T>
 using is_variant = detail::is_variant<T>;
 
+/**
+ * @ingroup Common
+ * @brief SFINAE type which is valid if the type is a variant.
+ * @related resilient::Variant
+ *
+ * @tparam T The type that should be a variant
+ */
 template<typename T>
 using if_is_variant = std::enable_if_t<is_variant<std::decay_t<T>>::value, void*>;
 
+/**
+ * @ingroup Common
+ * @brief Check whether the Variant contains a type
+ * @related resilient::Variant
+ * @see http://en.cppreference.com/w/cpp/utility/variant
+ */
 using detail::holds_alternative;
+
+/**
+ * @ingroup Common
+ * @brief Get the type contained by the variant
+ * @related resilient::Variant
+ * @see http://en.cppreference.com/w/cpp/utility/variant
+ */
 using detail::get;
+
+/**
+ * @ingroup Common
+ * @brief Calls a visitor with the type currently hold by the variant.
+ * @related resilient::Variant
+ * @see http://en.cppreference.com/w/cpp/utility/variant
+ */
 using detail::visit;
 
 }
