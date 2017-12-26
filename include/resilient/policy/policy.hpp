b558db58b81d20b49d3165f0e3ae8a9f6232d2d6
Francesco Zoffoli <makers.f.dev@gmail.com>
2017-12-26 11:42:20 +0000

    Added initial documentation to policy

--- include/resilient/policy/policy.hpp
+++ include/resilient/policy/policy.hpp
@@ -1,3 +1,23 @@
+/**
+ * @defgroup Policy
+ *
+ * @brief A policy describes how to execute a task.
+ *
+ * # Description
+ *
+ * When executing a task there are several ways to cope with failure.
+ * An application might require to try to execute the task again, another one might need to stop when running them too many times.
+ *
+ * A policy specifies the rules of executing a task.
+ *
+ * # The Concept
+ *
+ * A class to qualify for the `Policy` concept needs to define a `execute()` method.
+ * The method need to take a callable object (which implements the `Task` concept) and the arguments to invoke the callable with.
+ * It needs to return a Failable.
+ *
+ */
+
 #include <resilient/policy/circuitbreaker.hpp>
 #include <resilient/policy/noop.hpp>
 #include <resilient/policy/pipeline.hpp>
--- include/resilient/policy/policy_utils.hpp
+++ include/resilient/policy/policy_utils.hpp
@@ -20,26 +20,50 @@ struct add_failure_type<Variant<Failures...>>
     using type = Variant<Failures..., NewFailures...>;
 };
 
+/**
+ * @brief Given a `Failure`, add new failures to the type.
+ *
+ * If the `Failure` is a `Variant` of failures then the new failures are appended to the `Variant`.
+ * Otherwise define a `Varint` with the previous failure and the new failures.
+ *
+ * @tparam Failure The `Failure` to extend.
+ * @tparam NewFailures The new `Failure`s to add.
+ */
 template<typename Failure, typename... NewFailures>
 using add_failure_type_t = typename add_failure_type<Failure>::template type<NewFailures...>;
 
-// Given a Failable defines a new Failable which has the same value_type and
-// 1. A failure_type which is the concatenation of NewFailures to the variant if it is a variant
-// 2. A failure_type which is a variant of the old failure_type and the NewFailures
+/**
+ * @brief Extend a `Failable`'s `Failure` with a new type.
+ *
+ * Given a `Failable` defines a new Failable which has the same value_type and
+ * 1. A failure_type which is the concatenation of NewFailures to the variant if it is a variant.
+ * 2. A failure_type which is a variant of the old failure_type and the NewFailures.
+ *
+ * @tparam Failable The `Failable` whose failure_type needs to be extended.
+ * @tparam NewFailures The new `Failure`s to add to the `Failable`.
+ */
 template<typename Failable, typename... NewFailures>
 using add_failure_to_failable_t =
     resilient::Failable<add_failure_type_t<typename Failable::failure_type, NewFailures...>,
                         typename Failable::value_type>;
 
-template<typename T, typename... NewFailures>
-using add_failure_to_noref_t =
-    add_failure_to_failable_t<std::remove_reference_t<T>, NewFailures...>;
 
-// Determines the type of invoking F with Args if they are both passed as references.
+/**
+ * @brief Like `add_failure_to_failable_t`, but remove reference from the `Failable`.
+ */
+template<typename Failable, typename... NewFailures>
+using add_failure_to_noref_t =
+    add_failure_to_failable_t<std::remove_reference_t<Failable>, NewFailures...>;
+
+/**
+ * @brief Define the type returned by invoking `F` with `Args...` both as lvalues.
+ */
 template<typename F, typename... Args>
 using noforward_result_of_t = detail::invoke_result_t<F&, Args&...>;
 
-// Determines the type of invoking F with Args if they are both forwarded.
+/**
+ * @brief Define the type returned by invoking `F` with `Args...` both forwarded.
+ */
 template<typename F, typename... Args>
 using forward_result_of_t = detail::invoke_result_t<F, Args...>;
 } // namespace resilient
