e44f463579bbf05a61ab7440de7d80665af3850f
Francesco Zoffoli <makers.f.dev@gmail.com>
2018-06-02 19:57:59 +0100

    Removed top level headers which where including too much.

--- include/resilient.hpp
+++
@@ -1,3 +0,0 @@
-#include <resilient/detector/detector.hpp>
-#include <resilient/task/task.hpp>
-#include <reslient/policy/policy.hpp>
--- include/resilient/policy/policy.hpp
+++ include/resilient/policy/policy.hpp
@@ -17,9 +17,3 @@
  * It needs to return a Failable.
  *
  */
-
-#include <resilient/policy/circuitbreaker.hpp>
-#include <resilient/policy/noop.hpp>
-#include <resilient/policy/pipeline.hpp>
-#include <resilient/policy/ratelimiter.hpp>
-#include <resilient/policy/retry.hpp>
